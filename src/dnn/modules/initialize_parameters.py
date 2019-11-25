5ab12c552861f3b207da715e68831e280eec4904
Cristian Mardare <cristian.mardare.dev@gmail.com>
2019-11-25 20:23:09 +0200

    change parameter initialization to use the He et al., 2015 variant

--- src/dnn/modules/initialize_parameters.py
+++ src/dnn/modules/initialize_parameters.py
@@ -14,7 +14,7 @@ def initialize_parameters(layer_dims):
     L = len(layer_dims)            # number of layers in the network
 
     for l in range(1, L):
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2./layers_dims[l-1])
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
      
     return parameters
