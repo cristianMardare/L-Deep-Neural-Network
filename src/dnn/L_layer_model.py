bc2db47b991af1d1253298c8e5bf80c4f513a298
Cristian Mardare <cristian.mardare.dev@gmail.com>
2019-12-11 22:20:37 +0200

    fix dependencies in L-layer model

--- src/dnn/L_layer_model.py
+++ src/dnn/L_layer_model.py
@@ -1,9 +1,12 @@
-from dnn.modules import *
+import dnn.modules as dnn
+import numpy as np
+import matplotlib.pyplot as plt
 
-def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False, lambd = 0, keep_prob = 1):#lr was 0.009
+
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False, lambd=0, keep_prob=1):  # lr was 0.009
     """
     Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
-    
+
     Arguments:
     X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
     Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
@@ -13,43 +16,43 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
     print_cost -- if True, it prints the cost every 100 steps
     lambd -- regularization hyperparameter, scalar
     keep_prob - probability of keeping a neuron active during drop-out, scalar.
-    
+
     Returns:
     parameters -- parameters learnt by the model. They can then be used to predict.
     """
 
     np.random.seed(1)
     costs = []                         # keep track of cost
-    
+
     # Parameters initialization.
-    parameters = initialize_parameters_deep(layers_dims)
-    
+    parameters = dnn.initialize_parameters(layers_dims)
+
     # Loop (gradient descent)
     for i in range(0, num_iterations):
 
         # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        AL, caches = L_model_forward(X, parameters)
-        
+        AL, caches = dnn.L_model_forward(X, parameters)
+
         # Compute cost.
-        cost = compute_cost(AL, Y, layers_dims, parameters, lambd)
-    
+        cost = dnn.compute_cost(AL, Y, layers_dims, parameters, lambd)
+
         # Backward propagation.
-        grads = L_model_backward(AL, Y, caches)
- 
+        grads = dnn.L_model_backward(AL, Y, caches)
+
         # Update parameters.
-        parameters = update_parameters(parameters, grads, learning_rate)
-                
+        parameters = dnn.update_parameters(parameters, grads, learning_rate)
+
         # Print the cost every 100 training example
         if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
+            print("Cost after iteration %i: %f" % (i, cost))
         if print_cost and i % 100 == 0:
             costs.append(cost)
-            
+
     # plot the cost
     plt.plot(np.squeeze(costs))
     plt.ylabel('cost')
     plt.xlabel('iterations (per hundreds)')
     plt.title("Learning rate =" + str(learning_rate))
     plt.show()
-    
+
     return parameters
